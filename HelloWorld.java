2ba4ec0f3b2603c34a7fdb93f0f3146513ed1bd6
codinghustler7 <dagidegu184@gmail.com>
2025-11-07 14:00:41 -0500

    Added my line and personalized HelloWorld message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
    public static void main(String[] args){
-      System.out.println("Hello World!");
+      System.out.println("Hello, my name is Dagi!");
+      System.out.println("New line");
+
    }
 }
